1a9491f553ae601a63ee8c49fea182b26942dbca
asidmax <yemelyanenko.maxim@gmail.com>
2022-11-22 19:04:54 +0300

    update

--- simple/simplestart/forms.py
+++ simple/simplestart/forms.py
@@ -1,9 +1,11 @@
 from django.forms import ModelForm
+import os
 
 from .models import Ss
 
 
 class SsForm(ModelForm):
+
     class Meta:
         model = Ss
         fields = ('title', 'rubric', 'content', 'price')
